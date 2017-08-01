6186cf65ed99f131f8d766b9fef7e72879824159
Anya Craig <anya.craig@galepartners.com>
2017-08-01 14:09:45 -0400

    Removed functions from start project.

--- travel_bucket_start/api/destinations/controller.js
+++ travel_bucket_start/api/destinations/controller.js
@@ -6,28 +6,19 @@ var Destination = require('./model.js');
 // this function finds all the destinations
 // and then sends them back to whatever sent the request
 // which is our React App component
-exports.index = function(req,res){
-	Destination.find()
-	.then((destinations) => res.send(destinations));
-};
+
+// WRITE INDEX FUNCTION HERE
+
 
 // this function removes the destination with the specific id
 // we passed it (from our React App component)
 // then sends a message of "okay" when it's done
-exports.destroy = function(req, res){
-	Destination.remove({ _id: req.params.id })
-	.then(() => res.send("Okay"));
-};
+
+// WRITE DESTROY FUNCTION HERE
+
 
 // this function creates a new destination instance
 // adds the destination info to it, saves it
 // and then sends it back to the requester (our React App component)
-exports.create = function(req, res) {
-  var destination = new Destination();
-  destination.destinationName = req.body.destinationName;
-  destination.destinationImageUrl = req.body.destinationImageUrl;
-  destination.destinationLat = req.body.destinationLat;
-  destination.destinationLong = req.body.destinationLong;
-  destination.save()
-  .then(destination => res.send(destination));
-};
+
+// WRITE CREATE FUNCTION HERE
